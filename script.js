9c868f9582319b87bfd3946d4ea3123944340aa6
Renan <rocha.renan06@gmail.com>
2024-08-30 20:12:30 -0300

    comentários

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Posiciona o botão principal em uma posição aleatória ao carregar a página
     positionButtonRandomly(document.getElementById('mainButton'));
 });
 
 document.getElementById('mainButton').addEventListener('click', function() {
-    // Esconde o botão principal com efeito de fumaça
     this.classList.add('smoke-effect');
     setTimeout(() => {
         this.classList.add('hidden');
-    }, 2000); // Tempo da animação (2 segundos)
+    }, 2000);
 
-    // Exibe os outros botões
     setTimeout(() => {
         document.getElementById('buttonContainer').classList.remove('hidden');
-        // Posiciona o botão real de download aleatoriamente
         positionButtonRandomly(document.querySelector('.download-button'));
     }, 2000);
 });
@@ -25,13 +21,11 @@ function openUrl(buttonNumber) {
     }
 }
 
-// Função para mover o botão aleatoriamente dentro do container
 function moveButton(button) {
     const container = document.querySelector('.container');
     const containerRect = container.getBoundingClientRect();
     const buttonRect = button.getBoundingClientRect();
 
-    // Calcula novas posições aleatórias dentro do container
     const maxLeft = containerRect.width - buttonRect.width;
     const maxTop = containerRect.height - buttonRect.height;
     const randomLeft = Math.random() * maxLeft;
@@ -41,13 +35,11 @@ function moveButton(button) {
     button.style.top = `${randomTop}px`;
 }
 
-// Função para posicionar o botão de download aleatoriamente ao abrir
 function positionButtonRandomly(button) {
     const container = document.querySelector('.container');
     const containerRect = container.getBoundingClientRect();
     const buttonRect = button.getBoundingClientRect();
 
-    // Calcula novas posições aleatórias dentro do container
     const maxLeft = containerRect.width - buttonRect.width;
     const maxTop = containerRect.height - buttonRect.height;
     const randomLeft = Math.random() * maxLeft;
@@ -57,7 +49,6 @@ function positionButtonRandomly(button) {
     button.style.top = `${randomTop}px`;
 }
 
-// Mostrar a URL ao passar o mouse sobre os botões
 document.querySelectorAll('.fake-button').forEach((button) => {
     button.addEventListener('mouseover', function() {
         moveButton(button);
